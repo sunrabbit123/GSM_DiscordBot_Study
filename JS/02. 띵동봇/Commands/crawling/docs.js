8c89a86377295031a96d4e80ca6d691cb8cdd2d9
sunrabbit123 <qudwls185@naver.com>
2020-08-23 23:15:55 +0900

    fix typo

--- JS/02. 띵동봇/Commands/crawling/docs.js
+++ JS/02. 띵동봇/Commands/crawling/docs.js
@@ -10,11 +10,11 @@ exports.run = async (client, msg, args, prefix) => {
     if (!args[0]) return msg.reply("❎ 쿼리를 입력해주세요.") // argument가 없다면 쿼리를 입력해달라고 전해줍시다.
     let query = encodeURI(args.join(" ")) // 쿼리를 선언하였고 검색에 사용할 것이오니 인코드를 해줍시다.
     fetch(`https://djsdocs.sorta.moe/v1/main/stable/embed?q=${query}`).then(a => a.json().then((r) => { // 해당 api를 fetch 합시다 a = fetch한 값들, 그걸 json화 하고 그 값들을 불러오기 위해서는 다시 then을 사용하여 r로 선언합니다.
-        if (!r) return msg.reply("❎ 해당 쿼리는 존재하지 않습니다.")
+        if (!r) {return msg.reply("❎ 해당 쿼리는 존재하지 않습니다.")}
         const docs = new Discord.MessageEmbed(r) // docs를 const로 선언을 해줍시다. 
         docs.setFooter(msg.author.tag, msg.author.displayAvatarURL())
         msg.reply(docs)
-    }))
+    }));
 }
 
 exports.config = {
