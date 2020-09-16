63e5c1b20b9dc2e87ec29c069dcbc1d83ddc0933
Goolgae <sunwoo0706@icloud.com>
2020-09-16 23:02:58 +0900

    git reset

--- Golang/01.GOGO/app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-from app.main.index import main as main
-    
-app.register_blueprint(main)
-    
--- Python/01.Shaki/runsnow.py
+++
@@ -1,8 +0,0 @@
-from New_snowshaki import ShakiBot
-
-print("와ㅏㅏ 실행된다ㅏㅏ")
-ShakiBot().run("토큰")
-
-
-print("샤키가 사라졌다")
-
