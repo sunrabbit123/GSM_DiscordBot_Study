1f6f362b9c94e757dbf1bf316f162f72cb2ecdf9
sunrabbit123 <qudwls185@naver.com>
2020-10-08 15:24:41 +0900

    add plus_minus_date function

--- Python/01.Shaki/utils/date_converter.py
+++ Python/01.Shaki/utils/date_converter.py
@@ -8,13 +8,35 @@ def pattern_Comparison(pattern, text) -> bool:
         return True
     else:
         return False
+def plus_minus_date(date : datetime.datetime, YMWD, value : int):
+    if YMWD == 'Y':
+        if value > 0:
+            date += datetime.timedelta(days = 365 * value)
+        else :
+            value *= value
+            date -= datetime.timedelta(days = 365 * value)
+    elif YMWD == "M":
+        if value > 0:
+            date += datetime.timedelta(month = value)
+        else :
+            value *= value
+            date -= datetime.timedelta(month = value)
+    elif YMWD == "W"
+        if value > 0:
+            date += datetime.timedelta(weeks = value)
+        else :
+            value *= value
+            date -= datetime.timedelta(weeks = value)
+    else :
+        if value > 0:
+            date += datetime.timedelta(weeks = value)
+        else :
+            value *= value
+            date -= datetime.timedelta(weeks = value)  
 
-def set_date(text : str, YMWD : str, date : datetime.date, val : int = 1):
+def set_date(text : str, YMWD : str, date : datetime.datetime, val : int = 1):
     if(pattern_Comparison(re.compile(r'(전|저|지)'), text)):
-        date -= datetime.timedelta(days = 365 * val) if YMWD == 'Y' else \
-               datetime.timedelta(month = val) if YMWD == 'M' else \
-               datetime.timedelta(weeks = val) if YMWD == 'W' else \
-               datetime.timedelta(days = val)
+        date -= plus_minus_date(date, YMWD, val)
     elif(pattern_Comparison(re.compile(r'(후|뒤|다)'), text)):
         date += datetime.timedelta(days = 365 * val) if YMWD == 'Y' else \
                datetime.timedelta(month = val) if YMWD == 'M' else \
@@ -25,9 +47,6 @@ def set_date(text : str, YMWD : str, date : datetime.date, val : int = 1):
                datetime.timedelta(month = val) if YMWD == 'M' else \
                datetime.timedelta(weeks = val) if YMWD == 'W' else \
                datetime.timedelta(days = val)
-def set_Fixed_Date(val, YMWD, date : datetime.date):
-    if YMWD == 'Y':
-        
     
 
 
@@ -37,17 +56,16 @@ def set_Fixed_Date(val, YMWD, date : datetime.date):
 
 class get_date:
     def __init__(self, message : discord.Message ):
-        text = ' '.join(message.content.split()[1:])f
-        self.date = datetime.date.today()
+        text = ' '.join(message.content.split()[1:])
+        self.date = datetime.datetime.now()
 
         if pattern_Comparison(re.compile(r'(일|월|달)'), text) and\
             pattern_Comparison(re.compile(r'[0-9]'), text):
-            
+            print(1)
     
 
-    def format_date():
-        formatted = re.sub('[0*9',' ', self.date.isformat)
-        formatted = re.sub(
+    def format_date(self):
+        formatted = re.sub('[^0-9]','', self.date.__str__.split("T")[0])
         return formatted
     
         
