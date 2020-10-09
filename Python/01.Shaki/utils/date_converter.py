310e64be5c64934a726955915dab761de4ad87ce
sunrabbit123 <qudwls185@naver.com>
2020-10-09 19:26:43 +0900

    add set_date function

--- Python/01.Shaki/utils/date_converter.py
+++ Python/01.Shaki/utils/date_converter.py
@@ -8,6 +8,7 @@ def pattern_Comparison(pattern, text) -> bool:
         return True
     else:
         return False
+
 def plus_minus_date(date : datetime.datetime, YMWD, value : int):
     if YMWD == 'Y':
         if value > 0:
@@ -21,7 +22,7 @@ def plus_minus_date(date : datetime.datetime, YMWD, value : int):
         else :
             value *= value
             date -= datetime.timedelta(month = value)
-    elif YMWD == "W"
+    elif YMWD == "W":
         if value > 0:
             date += datetime.timedelta(weeks = value)
         else :
@@ -36,17 +37,11 @@ def plus_minus_date(date : datetime.datetime, YMWD, value : int):
 
 def set_date(text : str, YMWD : str, date : datetime.datetime, val : int = 1):
     if(pattern_Comparison(re.compile(r'(전|저|지)'), text)):
-        date -= plus_minus_date(date, YMWD, val)
+        plus_minus_date(date, YMWD, val * -1)
     elif(pattern_Comparison(re.compile(r'(후|뒤|다)'), text)):
-        date += datetime.timedelta(days = 365 * val) if YMWD == 'Y' else \
-               datetime.timedelta(month = val) if YMWD == 'M' else \
-               datetime.timedelta(weeks = val) if YMWD == 'W' else \
-               datetime.timedelta(days = val)
+        plus_minus_date(date, YMWD, val)
     else:
-        date += datetime.timedelta(days = 365 * val) if YMWD == 'Y' else \
-               datetime.timedelta(month = val) if YMWD == 'M' else \
-               datetime.timedelta(weeks = val) if YMWD == 'W' else \
-               datetime.timedelta(days = val)
+        plus_minus_date(date, YMWD, val)
     
 
 
@@ -59,13 +54,17 @@ class get_date:
         text = ' '.join(message.content.split()[1:])
         self.date = datetime.datetime.now()
 
-        if pattern_Comparison(re.compile(r'(일|월|달)'), text) and\
+        if pattern_Comparison(re.compile(r'(일|월|달|주)'), text) and\
             pattern_Comparison(re.compile(r'[0-9]'), text):
-            print(1)
+            YMWD = 'M' if re.compile(r'(월|달)', text) else\
+                   ('W' if re.compile(r'(주)', text) else 'D')
+                   
+            val = text.replace('[^0-9]', "")
+            set_date(text, YMWD, self.date, int(val))
     
 
     def format_date(self):
-        formatted = re.sub('[^0-9]','', self.date.__str__.split("T")[0])
+        formatted = re.sub('[^0-9]', '', self.date.__str__.split("T")[0])
         return formatted
     
         
