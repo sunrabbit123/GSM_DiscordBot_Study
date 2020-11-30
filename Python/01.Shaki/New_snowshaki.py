3cd5c86d91f0224d8564ac03c92feaf2beb7ca68
sunrabbit123 <qudwls185@naver.com>
2020-11-30 21:44:38 +0900

    Delete mysql session
    ADD prefix

--- Python/01.Shaki/New_snowshaki.py
+++ Python/01.Shaki/New_snowshaki.py
@@ -36,7 +36,7 @@ class ShakiBot(commands.Bot):
     def __init__(self, db, *, debug = False, admin : str = '508788780002443284'):
         self.debug = debug
         #self.dbmanger = dbmanger()
-        self.prefix =["샤키야","참수진","수진아","Shaki","shaki"]
+        self.prefix =["샤키야","참수진","수진아","Shaki","shaki", "사카린", "샤캬", "새끼", "스노우스키", "샤키", "수진"]
         self.prefixed = 0
         self.admin = admin
         self.db = custom_db(db)
