2a4cd534341ae543d03a8b31107cfa0da825a4fe
sunrabbit123 <qudwls185@naver.com>
2021-01-06 19:25:15 +0900

    MISC Rearrange

--- Python/01.Shaki/runsnow.py
+++ Python/01.Shaki/runsnow.py
@@ -7,17 +7,18 @@ import pymongo
 # db_path = os.environ['db']
 
 from configparser import ConfigParser
+
 config = ConfigParser()
-config.read('config.ini')
-token = config.get('default', 'TOKEN')
-admin = config.get('default', 'ADMIN')
-db_path = config.get('default', 'DB')
+config.read("config.ini")
+token = config.get("default", "TOKEN")
+admin = config.get("default", "ADMIN")
+db_path = config.get("default", "DB")
 
 if __name__ == "__main__":
     client = pymongo.MongoClient(db_path)
-    db = client.get_database('Shaki').Shaki_command
+    db = client.get_database("Shaki").Shaki_command
     print("complete connect db")
     print("와ㅏㅏ 실행된다ㅏㅏ")
-    ShakiBot(db, admin = admin).run(token)
+    ShakiBot(db, admin=admin).run(token)
 
 print("샤키가 사라졌다")
